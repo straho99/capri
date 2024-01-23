4d1294b2ed7751032dcd4f6c18d0c5f14b012285
StrahilRuychev <strahil.ruychev@gmail.com>
2024-01-23 16:02:24 +0200

    Initial Commit

---
+++ capri/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  capri
+//
+//  Created by Strahil Ruychev on 23.01.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
